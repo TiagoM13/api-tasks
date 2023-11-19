b239c66eaba2b8f0b49b7a5d2f707295fcf25ece
TiagoM13 <thyagomotha2000@gmail.com>
2023-11-19 12:05:44 -0300

    fix: port config

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "@app/app";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
   console.log(`🚀 HTTP server running on http://localhost:${PORT}`)
