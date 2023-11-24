5490e4d4beee675f7abfea50691f5922b6e7789a
TiagoM13 <thyagomotha2000@gmail.com>
2023-11-24 12:46:17 -0300

    fix: update PORT

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import { app } from "@app/app";
 
 if (process.env.NODE_ENV !== 'production') {
-  app.listen(process.env.PORT ?? 3333, () => {
-    console.log(`🚀 HTTP server running on http://localhost:${process.env.PORT ?? 3333}`)
+  app.listen(process.env.PORT || 3333, () => {
+    console.log(`🚀 HTTP server running on http://localhost:${process.env.PORT || 3333}`)
   })
 }
 
